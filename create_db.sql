82d6bd9e82112a2c35be75451970f08ef080ac53
Fernando <fcmendoza@gmail.com>
2020-02-22 18:47:34 -0800

    Added creation of House table and sproc to retrieve houses by hostID.

--- create_db.sql
+++ create_db.sql
@@ -27,21 +27,63 @@ ELSE BEGIN
 END
 GO
 
-declare @count int = 1
-while (@count < 10) BEGIN
-    insert into Host (Name) VALUES ('Jon' + cast(@count as varchar) + ' Host') 
-    set @count = @count + 1
+if not exists (select top 1 * from Host (nolock)) begin
+	declare @count int = 1
+	while (@count < 10) begin
+	    insert into Host (Name) VALUES ('Jon' + cast(@count as varchar) + ' Host')
+	    set @count = @count + 1
+	end
+end
+GO
+
+if not exists (select top 1 * from Guest (nolock)) begin
+	declare @count int = 1
+	set @count = 1
+	while (@count <= 10) begin
+	    insert into Guest (Name) VALUES ('Jon' + cast(@count as varchar) + ' Guest')
+	    set @count = @count + 1
+	end
+end
+GO
+
+-- Houses hosts have/own/manage.
+if not exists (select 1 from sys.tables where name = 'House') begin
+	CREATE TABLE House (
+		[ID] int identity(1,1) not null constraint [PK_House] primary key,
+		[HostId] int not null constraint [FK_House_Host] foreign key (HostId) references Host(ID),
+		[Address1] varchar(100) not null,
+		[Address2] varchar(100) null,
+		[City]     varchar(100) not null,
+		[State]    varchar(50)  not null,
+		[ZipCode]  char(5)      not null,
+		[CreatedOn] datetime not null CONSTRAINT [DF_House_CreatedOn] DEFAULT (GETUTCDATE())
+	)
+end
+GO
+
+if not exists (select top 1 * from House (nolock)) begin
+	declare @count int = 1
+	while (@count < 10) begin
+		INSERT INTO House (HostId, Address1, City, State, ZipCode)
+		VALUES (@count, '311' + cast(@count as varchar) + ' SACRAMENTO ST', 'SAN FRANCISCO', 'CA', '94115')
+	    set @count = @count + 1
+	end
+end
+
+if exists (select 1 from sys.procedures where name = 'GetHousesByHost') begin
+	DROP PROCEDURE GetHousesByHost
+end
+GO
+
+CREATE PROCEDURE GetHousesByHost (
+	@HostID int
+)
+AS
+BEGIN
+	SELECT h.ID, h.HostId, hs.Name, (h.Address1 + ', ' + h.City + ', ' + h.State + ' ' + h.ZipCode) as Address
+	FROM House h (nolock)
+	INNER JOIN Host hs (nolock) ON hs.ID = h.hostId
+	WHERE h.HostId = @HostID
 END
+GO
 
-set @count = 1
-while (@count <= 10) BEGIN
-    insert into Guest (Name) VALUES ('Jon' + cast(@count as varchar) + ' Guest') 
-    set @count = @count + 1
-END
-
-
-select top 10 * from Host (nolock)
-select top 10 * from Guest (nolock)
-
--- truncate table Host
--- truncate table Guest
